e9929ebe4c0107f81619740646397cbbaff3e530
Alpha <d.juzu7777@gmail.com>
2024-10-06 18:52:06 +0200

    Fix code erro in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import App from './app.jsx';
 import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root').render(<App/>));
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
