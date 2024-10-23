1713c82d48d8e1155ceeae2caf1f5036ad12278b
ikmiles <ikmiles83@gmail.com>
2024-10-24 00:27:50 +0200

    Did some adjustment to the footer and added the profile image

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,70 +1,72 @@
 import './styles/footer.css'
 import { FaChevronRight } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
-import { FaPhone } from "react-icons/fa6";
-import { FaFacebookF } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaPhone,FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF,FaInstagram  } from "react-icons/fa";
+import profile from '../styles/channels4_profile.jpg'
 const footer = () => {
   return (
     <div className='footer-container'>
-        <div className="section-1">
-            <div className="profile">
-                <img src="" alt="" />
-            </div>
-            <div className="email">
-                <h2>Subscribe</h2>
-                <div className="one">
-                    <div className="input">
-                        <IoIosMail className='mail' fontSize={"25px"}/>
-                        <input type="text" placeholder='Email here...' />
+        <div className="footer">
+            <div className="section-1">
+                <div className="profile">
+                    <img src={profile} alt="" />
+                </div>
+                <div className="email">
+                    <h2>Subscribe</h2>
+                    <div className="one">
+                        <div className="input-box">
+                            <input type="text" placeholder='Email here...' />
+                            <IoIosMail className='mail' fontSize={"25px"}/>
+                        </div>
+                        <button className='right-icon'><FaChevronRight /></button>
                     </div>
-                    <button className='right-icon'><FaChevronRight /></button>
                 </div>
             </div>
+            <div className="section-2">
+                <div className="details-1">
+                    <h3>About Us</h3>
+                    <ul>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Events</a></li>
+                    </ul>
+                </div>   
+                <div className="details-1">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Beat sales</a></li>
+                        <li><a href="#">Music production</a></li>
+                        <li><a href="#">Video production</a></li>
+                    </ul>
+                </div>   
+                <div className="details-1">
+                    <h3>Contact</h3>
+                    <ul>
+                        <div className="myemail">
+                            <li><a href="#"><IoIosMail className='mail' fontSize={"20px"}/> Miles@gmail.com</a></li>
+                        </div>
+                        <div className="myemail">
+                            <li><a href="#"><FaPhone  className='mail' fontSize={"20px"}/> +250-790342423</a></li>
+                        </div>
+                    
+                    </ul>
+                    <div className="socials">
+                        <div className="social-icon">
+                            <a href="#"></a>
+                            <FaFacebookF fontSize={"20px"}/>
+                        </div>
+                        <div className="social-icon">
+                            <a href="#"></a>
+                            <FaInstagram fontSize={"20px"}/>
+                        </div>
+                        <div className="social-icon">
+                            <a href="#"></a>
+                            <FaXTwitter fontSize={"20px"}/>
+                        </div>
+                    </div>
+                </div>
         </div>
-        <div className="section-2">
-            <div className="details-1">
-                <h3>About Us</h3>
-                <ul>
-                    <li><a href="#">Company</a></li>
-                    <li><a href="#">Career</a></li>
-                    <li><a href="#">Blogs</a></li>
-                    <li><a href="#">Events</a></li>
-                </ul>
-            </div>   
-            <div className="details-1">
-                <h3>Services</h3>
-                <ul>
-                    <li><a href="#">Beat sale</a></li>
-                    <li><a href="#">Press</a></li>
-                    <li><a href="#">Blogs</a></li>
-                    <li><a href="#">Audio production</a></li>
-                </ul>
-            </div>   
-            <div className="details-1">
-                <h3>Contact</h3>
-                <ul>
-                    <div className="myemail">
-                        <li><a href="#"><IoIosMail className='mail' fontSize={"20px"}/> miles@gmail.com</a></li>
-                    </div>
-                    <div className="myemail">
-                        <li><a href="#"><FaPhone  className='mail' fontSize={"20px"}/> +250-790342423</a></li>
-                    </div>
-                  
-                </ul>
-                <div className="socials">
-                    <div className="social-icon">
-                        <FaFacebookF fontSize={"20px"}/>
-                    </div>
-                    <div className="social-icon">
-                        <FaInstagram fontSize={"20px"}/>
-                    </div>
-                    <div className="social-icon">
-                        <FaXTwitter fontSize={"20px"}/>
-                    </div>
-                </div>
-            </div>
         </div>
 
     </div>
